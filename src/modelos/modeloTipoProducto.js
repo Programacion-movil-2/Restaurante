11b76794478bead612ec9fda5e16593ccd69b6bf
SamuelOviedo <isaaccoviedo@gmail.com>
2022-02-24 22:08:21 -0600

    Agregado de modelos producto y tipo producto

---
+++ src/modelos/modeloTipoProducto.js
@@ -0,0 +1,33 @@
+const sequelize = require('sequelize');
+const db = require('../configuracion/db');
+ 
+//Definimos los campos de las tablas
+const TipoProducto = db.define(
+   "tipoProducto",
+   {
+       idTipoProducto:{
+           type: sequelize.INTEGER,
+           primaryKey: true,
+           autoIncrement: true,
+           allowNull: false,
+       },
+ 
+       nombre:{
+           type: sequelize.STRING(45),
+           allowNull: false,
+       },
+     
+       idTipoPrincipal:{
+           type: sequelize.INTEGER,
+           allowNull: true,
+       },
+   },
+ 
+   {
+       tableName: "tipoProducto",
+       timestamps: false,
+   }
+);
+ 
+//Exportamos este modelo
+module.exports = TipoProducto;
