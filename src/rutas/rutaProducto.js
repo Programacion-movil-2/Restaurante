1e304223aff0925647a273984ae62e01133d401f
abdiel Giron <abdielgiron32@gmail.com>
2022-03-17 16:40:39 -0600

    creacion de metodos de categorias

--- src/rutas/rutaProducto.js
+++ src/rutas/rutaProducto.js
@@ -8,5 +8,6 @@ router.get('/listar', controladorProducto.listarProductos);
 router.post('/guardar', controladorProducto.guardar);
 router.put('/modificar', controladorProducto.modificar);
 router.put('/eliminar', controladorProducto.eliminar);
+router.get('/listarProductosTipo', controladorProducto.listarProductosPorTipo);
 
 module.exports = router;
